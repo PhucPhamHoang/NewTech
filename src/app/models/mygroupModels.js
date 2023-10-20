f3b0eadbe61f750a0407c395f7a329437836c2d3
PhucPhamHoang <phuclateo@gmail.com>
2023-10-20 22:43:01 +0700

    week4

---
+++ src/app/models/mygroupModels.js
@@ -0,0 +1,6 @@
+const mygroup = [
+    {id: "19110152", name:'Huynh Gia Kien'},
+    {id: "19110147", name:'Pham Hoang Phuc'},
+    {id: "19110132", name:'Nguyen Hoang Sang'},
+]
+module.exports = { mygroup }
